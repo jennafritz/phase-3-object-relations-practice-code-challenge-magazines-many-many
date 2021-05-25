5edb98fc46d12ad2ca4843ed8e1bb8205387fe31
jennafritz <jennafritz24@gmail.com>
2021-05-25 10:38:02 -0400

    Complete

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,3 +1,24 @@
+require 'pry'
+
 class Magazine < ActiveRecord::Base
-  
+    has_many :subscriptions
+    has_many :readers, through: :subscriptions
+
+    def email_list
+        email_array = self.readers.map do |reader_instance|
+            reader_instance.email
+        end
+        email_array.join(";")
+    end
+
+    def subscriber_count
+        self.subscriptions.count
+    end
+
+    def self.most_popular
+        subscribes_array = self.all.sort do |magazine_instance_a, magazine_instance_b|
+            magazine_instance_a.subscriber_count <=> magazine_instance_b.subscriber_count
+        end
+        subscribes_array.last
+    end
 end
--- app/models/reader.rb
+++ app/models/reader.rb
@@ -1,3 +1,21 @@
 class Reader < ActiveRecord::Base
-  
+    has_many :subscriptions
+    has_many :magazines, through: :subscriptions
+
+    def subscribe(magazine, price)
+        new_sub = Subscription.create(price: price, magazine_id: magazine.id, reader_id: self.id)
+        new_sub
+    end
+
+    def total_subscription_price
+        total = 0
+        self.subscriptions.map do |subscription_instance|
+            total = total + subscription_instance.price
+        end
+        total
+    end
+
+    def cancel_subscription(magazine)
+        self.subscriptions.find_by(magazine_id: magazine.id).destroy
+    end
 end
